0911347c9e29cb0e83243894b50f444de86f2549
Hyggesen <69785485+Hyggesen@users.noreply.github.com>
2021-04-16 11:39:52 +0200

    fetcing API from WooCommerce now works

---
+++ js/productsapi.js
@@ -0,0 +1,45 @@
+const url ="https://rainydays.benjamin-flower.store/wp-json/wc/v3/products";
+const key = "consumer_key=ck_d73cda14f037b09c0c4c3ec694d69287d6ce6f33";
+const secret = "consumer_secret=cs_d39990c64d590ccc8f9f6711ea8df93dd0568733";
+
+const wooApi =`${url}?${key}&${secret}`;
+
+const productContainer = document.querySelector (".products");
+const featuredContainer = document.querySelector(".featured");
+
+async function getProducts () {
+
+    try {
+        const response = await fetch(wooApi);
+        const getResults =await response.json();
+        createHtml(getResults);
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+}
+
+getProducts();
+
+function createHtml (products) {
+    
+    products.forEach(function(product){
+
+const productHtml = 
+`<div class="card">
+<img src="${product.images[0].src}" />
+<div class="bottom-card">
+     <p class="text">${product.name}</p>
+     <p class="title">${product.attributes[0].options[0]}</p>
+     <p class="price">${product.price}</p> 
+    <a class="details-button" href="productpage.html?id=${product.id}">Details</a>
+</div>
+</div>`
+
+    productContainer.innerHTML += productHtml;
+    featuredContainer.innerHTML += productHtml;
+
+    })
+        
+    }
---
+++ productpage.js
@@ -0,0 +1,90 @@
+const productContainer = document.querySelector(".productDetails");
+
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+console.log(params)
+
+const id = params.get("id");
+console.log(id);
+
+
+const url ="https://rainydays.benjamin-flower.store/wp-json/wc/v3/products/";
+const key = "consumer_key=ck_d73cda14f037b09c0c4c3ec694d69287d6ce6f33";
+const secret = "consumer_secret=cs_d39990c64d590ccc8f9f6711ea8df93dd0568733";
+
+const product = `${url}${id}?${key}&${secret}`;
+
+async function getSingleProduct() {
+    try {
+      const response = await fetch(product);
+      const json = await response.json();
+  
+      createProductHtml(json);
+    } catch (error) {
+      productContainer.innerHTML = displayError(
+        "We could not find this product right now, please try again."
+      );
+    }
+  }
+  getSingleProduct();
+  
+  function createProductHtml(product) {
+      productContainer.innerHTML = `
+      <section class="product-page-main">
+
+      <div class="product-image-container">
+
+        <div class="leftarrow">
+          <i class="fas fa-angle-left"></i>
+        </div>
+        
+        <div class="middlejacket">
+        <img src="${product.images[0].src}" />
+        </div>
+        
+        <div class="rightarrow">
+        <i class="fas fa-angle-right"></i>
+      </div>
+      </div>
+
+      <div class="product-description-container">
+        <h1>${product.name}</h1>
+        <span>${product.price}</span>
+        <p>This jacket from ${product.attributes[0].options[0]} is a great choice.
+        </p>
+        <span class="instock"><i class="fas fa-check"></i><p>In stock</p></span>
+        
+
+      <div class="choose-color">
+        <label for="color"></label>
+        <select name="color" id="color">
+          <option value="" selected disabled hidden>Choose Color</option>
+          <option value="Yellow">Yellow</option>
+          <option value="Red">Red</option>
+          <option value="Black">Black</option>
+        </select>
+      </div>
+
+        
+    <div class="choose-size">
+      <label for="size"></label>
+      <select name="size" id="size" value="test">
+        <option value="" selected disabled hidden>Choose Size</option>
+        <option value="Yellow">40</option>
+        <option value="Red">50</option>
+        <option value="Black">60</option>
+      </select>
+
+</div>
+    <div class="button-wrapper">
+         <p class="addtocart-button" onclick="addToCart()">Add to cart</p>
+       <a class="buynow-button" href="checkoutpage.html">Buy Now</a>
+    </div>   
+
+
+      </div>
+
+
+    </section>
+     `;
+  }
