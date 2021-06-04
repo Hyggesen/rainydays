4a9ea42725e1a060775ccb877c9c7625e4a80a8e
Hyggesen <69785485+Hyggesen@users.noreply.github.com>
2021-06-04 12:33:51 +0200

    product description/specs/size guide dropdown function added

---
+++ js/featured.js
@@ -0,0 +1,54 @@
+const aurl ="https://rainydays.benjamin-flower.store/wp-json/wc/v3/products";
+const akey = "consumer_key=ck_d73cda14f037b09c0c4c3ec694d69287d6ce6f33";
+const asecret = "consumer_secret=cs_d39990c64d590ccc8f9f6711ea8df93dd0568733";
+const awooApi =`${aurl}?${akey}&${asecret}`;
+const featuredContainer = document.querySelector(".featured");
+
+async function getProducts () {
+
+    try {
+        const response = await fetch(awooApi);
+        jacket =await response.json();
+        createFeaturedHtml(jacket);
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+}
+
+getProducts();
+
+
+
+    function createFeaturedHtml (product) {
+
+    
+        for ( i=0; i < 4; i++) {
+
+            featuredContainer.innerHTML +=     `<div class="card">
+            <img class="product-image-front" src="${product[i].images[0].src}" />
+            <div class="bottom-card">
+                 <p class="text">${product[i].name}</p>
+                 <p class="title">${product[i].attributes[0].options[0]}</p>
+                 <p class="price">${product[i].price}</p> 
+                <a class="details-button" href="productpage.html?id=${product[i].id}">Details</a>
+            </div>
+            </div>`;
+        }
+    
+       
+    
+    
+        }
+
+        createFeaturedHtml();
+
+
+        
+
+
+
+
+
+
---
+++ js/toggleproductspecs.js
@@ -0,0 +1,42 @@
+var desContent = document.querySelector("");
+var arrowIcon = document.getElementById("");
+ 
+
+function openDescription() {
+    desContent = document.querySelector(".product-description__content");
+    arrowIcon = document.getElementById("description-arrow");
+    changeContent();
+}
+
+
+function openTechSpecs() {
+    desContent = document.querySelector(".technical-spesification__content");
+    arrowIcon = document.getElementById("technical-arrow");
+    changeContent();
+
+}
+
+
+function openSizeguide() {
+    desContent = document.querySelector(".size-guide__content");
+    arrowIcon = document.getElementById("size-arrow");
+    changeContent();
+
+}
+
+
+
+function changeContent() {
+    if (desContent.style.display === "none") {
+        desContent.style.display = "block";
+       
+        arrowIcon.classList.remove("fa-chevron-right");
+        arrowIcon.classList.add("fa-chevron-down");
+
+
+      } else {
+        desContent.style.display = "none";
+        arrowIcon.classList.remove("fa-chevron-down");
+        arrowIcon.classList.add("fa-chevron-right");
+      }
+}
